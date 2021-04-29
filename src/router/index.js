2591e4fe3b06c942608bef2475c5e859c4c503b9
avishj <avish.j@protonmail.com>
2021-04-29 16:00:32 +0530

    router

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,51 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+	{
+		path: '/',
+		name: 'Home',
+		component: Home
+	},
+	{
+		path: '/about',
+		name: 'About',
+		component: () =>
+			import(/* webpackChunkName: "about" */ '../views/About.vue')
+	},
+	{
+		path: '/contact',
+		name: 'Contact',
+		component: () =>
+			import(/* webpackChunkName: "about" */ '../views/Contact.vue')
+	},
+	{
+		path: '/courses',
+		name: 'Courses',
+		component: () =>
+			import(/* webpackChunkName: "about" */ '../views/Courses.vue')
+	},
+	{
+		path: '/ceo-message',
+		name: 'CEO Message',
+		component: () =>
+			import(/* webpackChunkName: "about" */ '../views/CEO-Message.vue')
+	},
+	{
+		path: '/team',
+		name: 'Team',
+		component: () =>
+			import(/* webpackChunkName: "about" */ '../views/Team.vue')
+	}
+];
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
+	mode: 'history',
+	base: process.env.BASE_URL,
+	routes
+});
 
-export default router
+export default router;
