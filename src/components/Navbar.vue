ef651c6af983442fd08292833afaf0a373c9564a
avishj <avish.j@protonmail.com>
2021-04-29 19:57:39 +0530

    mobile-fix

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,58 +1,57 @@
 <template>
-	<div>
-		<b-navbar type="ants-blue" variant="faded">
-			<b-navbar-nav>
-				<b-navbar-brand to="/">
-					<b-img
-						height="70px"
-						:src="require('/src/assets/img/logo.png')"
-					></b-img>
-				</b-navbar-brand>
-			</b-navbar-nav>
-			<b-navbar-nav class="ml-auto h5 text-ants-blue">
-				<b-nav-item class="p-3" to="/">Home</b-nav-item>
-				<b-nav-item class="p-3" to="/team">Team</b-nav-item>
-				<b-nav-item class="p-3" to="/vision">Vision</b-nav-item>
-				<b-nav-item class="p-3" to="/ceo-message"
-					>CEO Message</b-nav-item
-				>
-				<b-nav-item class="p-3" to="/courses">Courses</b-nav-item>
-				<b-nav-item class="p-3" to="/contact">Contact Us</b-nav-item>
-			</b-navbar-nav>
-		</b-navbar>
-	</div>
+  <div>
+    <b-navbar toggleable="lg" type="ants-blue" variant="faded">
+      <b-navbar-brand to="/">
+        <b-img height="70px" :src="require('/src/assets/img/logo.png')"></b-img>
+      </b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto h5 text-ants-blue">
+          <b-nav-item class="p-3" to="/">Home</b-nav-item>
+          <b-nav-item class="p-3" to="/team">Team</b-nav-item>
+          <b-nav-item class="p-3" to="/vision">Vision</b-nav-item>
+          <b-nav-item class="p-3" to="/ceo-message">CEO Message</b-nav-item>
+          <b-nav-item class="p-3" to="/courses">Courses</b-nav-item>
+          <b-nav-item class="p-3" to="/contact">Contact Us</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
 </template>
 
 <style scoped>
+.navbar-ants-blue .navbar-toggler-icon {
+  background-image: url("data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgba(255,102,203, 0.5)' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 8h24M4 16h24M4 24h24'/%3E%3C/svg%3E");
+}
 .navbar-ants-blue {
-	background-color: white;
+  background-color: white;
 }
 /* changing brand text color*/
 .navbar-ants-blue .navbar-brand,
 .navbar-ants-blue .navbar-text {
-	color: #256fb4;
+  color: #256fb4;
 }
 /* navigation link text color */
 .navbar-ants-blue .navbar-nav .nav-link {
-	color: #256fb4;
+  color: #256fb4;
 }
 /* navigation links anchor hover and active styles */
 .navbar-ants-blue .nav-item.active .nav-link,
 .navbar-ants-blue .nav-item:hover .nav-link {
-	color: #358fd4;
+  color: #358fd4;
 }
 .navbar-ants-blue .navbar-nav .nav-link::after {
-	color: #358fd4;
-	content: '';
-	display: block;
-	width: 0;
-	height: 3px;
-	background: #358fd4;
-	transition: width 0.3s;
+  color: #358fd4;
+  content: "";
+  display: block;
+  width: 0;
+  height: 3px;
+  background: #358fd4;
+  transition: width 0.3s;
 }
 .navbar-ants-blue .nav-item.active .nav-link::after,
 .navbar-ants-blue .nav-item:hover .nav-link::after {
-	color: #358fd4;
-	width: 100%;
+  color: #358fd4;
+  width: 100%;
 }
 </style>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,155 +1,141 @@
 <template>
-	<div>
-		<div class="bg-ants-purple">
-			<b-row no-gutters>
-				<b-col
-					align-self="center"
-					class="text-white mx-7rem"
-					style="font-family: 'Rajdhani', sans-serif"
-				>
-					<h1>Contact Us</h1>
-					<h2>How can we help you out?</h2>
-				</b-col>
-				<b-col class="my-2 d-flex justify-content-center">
-					<b-img
-						fluid
-						:src="
+  <div>
+    <div class="bg-ants-purple">
+      <b-row no-gutters>
+        <b-col
+          align-self="center"
+          class="text-white mx-7rem"
+          style="font-family: 'Rajdhani', sans-serif"
+          md="6"
+        >
+          <h1>Contact Us</h1>
+          <h2>How can we help you out?</h2>
+        </b-col>
+        <b-col class="my-2 p-1 d-flex justify-content-center" md="6">
+          <b-img fluid :src="
 							require('/src/assets/img/contact-header-right.png')
-						"
-					></b-img>
-				</b-col>
-			</b-row>
-		</div>
-		<div style="background-color: #eeeeee">
-			<b-row no-gutters>
-				<b-col cols="6">
-					<b-form
-						@submit="onSubmit"
-						class="form-spl p-5 my-5 mx-7rem"
-					>
-						<b-form-group
-							id="input-group-1"
-							label="Email Address:"
-							label-for="input-1"
-							description="We'll never share your email with anyone else."
-						>
-							<b-form-input
-								id="input-1"
-								v-model="form.email"
-								type="email"
-								class="inp-spl"
-								placeholder="Enter email"
-								required
-							></b-form-input>
-						</b-form-group>
+						"></b-img>
+        </b-col>
+      </b-row>
+    </div>
+    <div style="background-color: #eeeeee">
+      <b-row no-gutters>
+        <b-col md="6">
+          <b-form @submit="onSubmit" class="form-spl p-5 my-5 mx-7rem">
+            <b-form-group
+              id="input-group-1"
+              label="Email Address:"
+              label-for="input-1"
+              description="We'll never share your email with anyone else."
+            >
+              <b-form-input
+                id="input-1"
+                v-model="form.email"
+                type="email"
+                class="inp-spl"
+                placeholder="Enter email"
+                required
+              ></b-form-input>
+            </b-form-group>
 
-						<b-form-group
-							id="input-group-2"
-							label="Subject:"
-							label-for="input-2"
-						>
-							<b-form-input
-								id="input-2"
-								class="inp-spl"
-								v-model="form.subject"
-								placeholder="Enter subject"
-								required
-							></b-form-input>
-						</b-form-group>
+            <b-form-group id="input-group-2" label="Subject:" label-for="input-2">
+              <b-form-input
+                id="input-2"
+                class="inp-spl"
+                v-model="form.subject"
+                placeholder="Enter subject"
+                required
+              ></b-form-input>
+            </b-form-group>
 
-						<b-form-group
-							id="input-group-3"
-							label="Full Description:"
-							label-for="input-3"
-						>
-							<b-form-textarea
-								id="input-3"
-								class="inp-spl"
-								v-model="form.desc"
-								placeholder="Enter the description"
-								required
-							>
-							</b-form-textarea>
-						</b-form-group>
+            <b-form-group id="input-group-3" label="Full Description:" label-for="input-3">
+              <b-form-textarea
+                id="input-3"
+                class="inp-spl"
+                v-model="form.desc"
+                placeholder="Enter the description"
+                required
+              ></b-form-textarea>
+            </b-form-group>
 
-						<b-button
-							type="submit"
-							variant="ants-purple"
-							class="btn-spl"
-							>Submit</b-button
-						>
-					</b-form>
-				</b-col>
-				<b-col align-self="center" class="text-center" cols="6">
-					<div class="div-spl mx-7rem">
-						<p class="font-weight-bold">
-							We're here to help you out.
-						</p>
-						<p>
-							If you have any questions, just reach out to us and
-							we’ll respond as soon as we can. Please provide as
-							much information as possible.
-						</p>
-					</div>
-					<br />
-					<br />
-					<br />
-					<div class="div-spl mx-7rem">
-						<p class="font-weight-bold">
-							You can also contact us through:
-						</p>
-						<p class="h6 font-weight-bold text-ants-purple">
-							info@antsupskill.com <br />+91 97718 39439
-						</p>
-					</div>
-				</b-col>
-			</b-row>
-		</div>
-	</div>
+            <b-button type="submit" variant="ants-purple" class="btn-spl">Submit</b-button>
+          </b-form>
+        </b-col>
+        <b-col align-self="center" class="text-center" md="6">
+          <div class="div-spl mx-7rem">
+            <p class="font-weight-bold">We're here to help you out.</p>
+            <p>
+              If you have any questions, just reach out to us and
+              we’ll respond as soon as we can. Please provide as
+              much information as possible.
+            </p>
+          </div>
+          <br />
+          <br />
+          <div class="div-spl mx-7rem">
+            <p class="font-weight-bold">You can also contact us through:</p>
+            <p class="h6 font-weight-bold text-ants-purple">
+              info@antsupskill.com
+              <br />+91 97718 39439
+            </p>
+          </div>
+          <br />
+        </b-col>
+      </b-row>
+    </div>
+  </div>
 </template>
 
 <style scoped>
 .form-spl {
-	background-color: white;
-	border-radius: 5px;
-	font-family: 'Rajdhani', sans-serif;
+  background-color: white;
+  border-radius: 5px;
+  font-family: "Rajdhani", sans-serif;
 }
 .btn-spl {
-	margin: 0 auto;
-	display: block;
-	padding-inline: 2rem;
+  margin: 0 auto;
+  display: block;
+  padding-inline: 2rem;
 }
-.mx-7rem {
-	margin-left: 7rem;
-	margin-right: 7rem;
+@media (min-width: 769px) {
+  .mx-7rem {
+    margin-left: 7rem;
+    margin-right: 7rem;
+  }
+}
+@media (max-width: 768px) {
+  .mx-7rem {
+    margin-left: 1rem;
+    margin-right: 1rem;
+  }
 }
 .inp-spl {
-	background-color: #eeeeee;
-	border-color: #eeeeee;
+  background-color: #eeeeee;
+  border-color: #eeeeee;
 }
 .div-spl {
-	border: 2px solid #5d56c3;
-	padding: 1rem;
+  border: 2px solid #5d56c3;
+  padding: 1rem;
 }
 </style>
 
 <script>
 export default {
-	name: 'Contact',
-	data() {
-		return {
-			form: {
-				email: '',
-				subject: '',
-				desc: ''
-			}
-		};
-	},
-	methods: {
-		onSubmit(event) {
-			event.preventDefault();
-			alert(JSON.stringify(this.form));
-		}
-	}
+  name: "Contact",
+  data() {
+    return {
+      form: {
+        email: "",
+        subject: "",
+        desc: ""
+      }
+    };
+  },
+  methods: {
+    onSubmit(event) {
+      event.preventDefault();
+      alert(JSON.stringify(this.form));
+    }
+  }
 };
 </script>
